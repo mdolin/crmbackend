4a3f7963158a6971cf87a2ef791f7fa2ceb30923
mdolin <mihadlnr@gmail.com>
2023-04-24 10:39:14 +0200

    update on handlers

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -11,7 +11,6 @@ import (
 )
 
 // Static HTML welcome page /
-
 func Index(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
